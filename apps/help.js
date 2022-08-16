13df4054892d29c07e1bebec13ac5093e3e917da
Windoge <254768511@qq.com>
2022-08-16 17:09:13 +0800

    更新帮助

--- apps/help.js
+++ apps/help.js
@@ -19,7 +19,7 @@ const path_ = `/plugins/xiaoyao-cvs-plugin-lite/resources/common/layout/`;
 export async function help(e, {
 	render
 }) {
-	if (!/图鉴/.test(e.msg) && !Cfg.get("sys.help", false)) {
+	if (!/便签/.test(e.msg) && !Cfg.get("sys.help", false)) {
 		return false;
 	}
 	let custom = {},
--- resources/help/help-cfg_default.js
+++ resources/help/help-cfg_default.js
@@ -1,61 +1,53 @@
 /*
 * 请勿直接修改此文件，可能会导致后续更新冲突
 * 如需自定义可将文件复制一份，重命名为 help-cfg.js 后编辑
+* 以下地址为映射路径
+* xiaoyao-cvs-plugin-lite/resources/help/help-cfg_default.js
 * */
 
 // 帮助配置
 export const helpCfg = {
-  title: "图鉴帮助",  // 帮助标题
-  subTitle: "Yunzai-Bot & xiaoyao-cvs-plugin-lite" // 帮助副标题
-};
-export const helpList = [{
-	"group": "信息查询",
-	"list": [{
-			"icon": 61,
-			"title": "#角色图鉴",
-			"desc": "目前已知原神角色概况"
-		},
-		{
-			"icon": 66,
-			"title": "#武器图鉴",
-			"desc": "目前已知原神武器概况"
-		},
-		{
-			"icon": 71,
-			"title": "#食谱图鉴",
-			"desc": "目前已知原神食谱概况"
-		},
-		{
-			"icon": 74,
-			"title": "#怪物图鉴",
-			"desc": "目前已知原神怪物概况"
-		},
-		{
-			"icon": 63,
-			"title": "#圣遗物图鉴",
-			"desc": "目前已知原神圣遗物概况"
-		},
-		{
-			"icon": 14,
-			"title": "#体力",
-			"desc": "重写原有的查询当前米游社体力"
-		},
-	]
-},{
-	"group": "管理命令，仅管理员可用",
-	"auth": "master",
-	"list": [{
-			"icon": 57,
-			"title": "#图鉴设置",
-			"desc": "例如#图鉴设置体力开启（关闭）"
-		},{
-			"icon": 58,
-			"title": "#图鉴更新",
-			"desc": "用于获取最新的图鉴数据图"
-		},{
-			"icon": 60,
-			"title": "#图鉴插件更新",
-			"desc": "用于获取最新的插件包数据"
-		}
-	]
-}]
+	title: "便签帮助",  // 帮助标题
+	subTitle: "Yunzai-Bot & xiaoyao-cvs-plugin-lite" // 帮助副标题
+  };
+  export const helpList = [{
+	  "group": "信息查询",
+	  "list": [{
+			  "icon": 1,
+			  "title": "#便签模板列表",
+			  "desc": "显示当前可用的模板资源"
+		  },
+		  {
+			  "icon": 2,
+			  "title": "#便签模板设置申鹤1",
+			  "desc": "根据上条命令数据，发送指定模板名称"
+		  },
+  
+	  ]
+  },{
+	  "group": "管理命令，仅管理员可用",
+	  "auth": "master",
+	  "list": [{
+			  "icon": 5,
+			  "title": "便签插件（强制）更新",
+			  "desc": "更新便签插件"
+		  },{
+			  "icon": 7,
+			  "title": "#便签背景图更新",
+			  "desc": "用于获取最新的便签模板图"
+		  },{
+			  "icon": 8,
+			  "title": "#导入便签模板图+数字",
+			  "desc": "使用指定的模板图模板资源"
+		  },{
+			  "icon": 9,
+			  "title": "#清空便签背景图",
+			  "desc": "清空便签背景图资源"
+		  },{
+			  "icon": 10,
+			  "title": "#便签设置",
+			  "desc": "修改便签相关设置内容"
+		  }
+	  ]
+  }]
+  
